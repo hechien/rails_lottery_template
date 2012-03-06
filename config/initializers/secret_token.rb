a0c833aaf917dcee4673ef753d7254025aaa3342
HeChien Hsu <hechien@me.com>
2012-03-06 19:53:34 +0800

    First initialize.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LotteryEvent::Application.config.secret_token = '9cdc72f1306a69372ac1687f209e7cdbfd1f49e53025f02d0e7017b098d5d09a9007f9ebd634d31adf367d84a73e0aaf4e86068ab4c440d4759025ce1c785701'
