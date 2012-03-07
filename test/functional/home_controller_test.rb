e04a68220fd0fb6db9191c05739af2037adeb9d8
HeChien Hsu <hechien@me.com>
2012-03-07 20:55:26 +0800

    [Add] Controller: HomeController

---
+++ test/functional/home_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get play" do
+    get :play
+    assert_response :success
+  end
+
+end
