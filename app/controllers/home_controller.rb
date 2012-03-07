fd529d26540e00eca1c43f42de4ec0c214f4b683
HeChien Hsu <hechien@me.com>
2012-03-07 21:08:33 +0800

    基本的骰子運作模式

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,24 @@
 class HomeController < ApplicationController
+  CURRENT_STAGE = 1
   def index
   end
 
   def play
+    if LotteryLog.user_can_play?(params[:options])
+      options = params[:options]
+      # Random
+      result = roll_the_dice
+      LotteryLog.create({
+        fb_uid: options[:fb_uid],
+        fb_email: options[:fb_email],
+        stage: CURRENT_STAGE,
+        win: result
+      })
+    end
+  end
+  
+  private
+  def roll_the_dice
+    rand * 200 < 10
   end
 end
