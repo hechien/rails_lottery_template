a8eacf27b013708806c7a9dd90a403494a339f13
HeChien Hsu <hechien@me.com>
2012-03-07 21:02:48 +0800

    [Add] LotteryLog#user_can_play?

--- app/models/lottery_log.rb
+++ app/models/lottery_log.rb
@@ -1,2 +1,12 @@
 class LotteryLog < ActiveRecord::Base
+  
+  def self.user_can_play?(options)
+    fb_uid, fb_email, stage = options.delete(:fb_uid), options.delete(:fb_email), options.delete(:stage)
+    result = where({
+      fb_uid: fb_uid,
+      fb_email: fb_email,
+      stage: stage
+    }).count
+    result < 1
+  end
 end
