8254ae3670f96dbe4830dfff33bbb75f41e790d7
HeChian <hechian@kdanmobile.com>
2012-03-07 23:28:18 +0800

    不應使用delete

--- app/models/lottery_log.rb
+++ app/models/lottery_log.rb
@@ -1,7 +1,7 @@
 class LotteryLog < ActiveRecord::Base
   
   def self.user_can_play?(options)
-    fb_uid, fb_email, stage = options.delete(:fb_uid), options.delete(:fb_email), options.delete(:stage)
+    fb_uid, fb_email, stage = options[:fb_uid], options[:fb_email], options[:stage]
     result = where({
       fb_uid: fb_uid,
       fb_email: fb_email,
