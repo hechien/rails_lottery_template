d6402eb941e67060d00f2dbc704045812d4667c8
HeChian <hechian@kdanmobile.com>
2012-03-08 20:39:49 +0800

    增加次數判定機制

--- app/models/lottery_log.rb
+++ app/models/lottery_log.rb
@@ -1,12 +1,21 @@
 class LotteryLog < ActiveRecord::Base
+  MAX_WINNER_COUNT = [5, 2, 3]
+  
+  def self.winner_in_stage(stage)
+    where({stage: stage}).count(:id)
+  end
   
   def self.user_can_play?(options)
     fb_uid, fb_email, stage = options[:fb_uid], options[:fb_email], options[:stage]
-    result = where({
-      fb_uid: fb_uid,
-      fb_email: fb_email,
-      stage: stage
-    }).count
-    result < 1
+    if MAX_WINNER_COUNT[(stage-1)] > self.winner_in_stage(stage)
+      result = where({
+        fb_uid: fb_uid,
+        fb_email: fb_email,
+        stage: stage
+      }).count
+      result < 1
+    else
+      return false
+    end
   end
 end
